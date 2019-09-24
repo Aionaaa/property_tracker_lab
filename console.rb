af499d3d12cd5f59c3169e34a39616d4196f6220
Iona Grant <ionagrant92@gmail.com>
2019-09-24 14:29:04 +0100

    save, read and delete_one methods added

--- console.rb
+++ console.rb
@@ -1,2 +1,25 @@
 require('pry')
 require_relative('models/property_tracker.rb')
+
+property1 = Property.new({
+  "address" => "10 Downing Street",
+  "no_of_bedrooms" => 6,
+  "year_built" => 1684,
+  "buy_or_let" => "let"
+  })
+
+  property1.save()
+
+  property2 = Property.new({
+    "address" => "Buckingham Palace",
+    "no_of_bedrooms" => 240,
+    "year_built" => 1703,
+    "buy_or_let" => "buy"
+    })
+
+    property2.save()
+
+    properties = Property.all()
+
+    binding.pry
+    nil
--- models/property_tracker.rb
+++ models/property_tracker.rb
@@ -13,6 +13,37 @@ class Property
     @buy_or_let = options['buy_or_let']
   end
 
+  def save()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "
+    INSERT INTO property_tracker
+    (address, no_of_bedrooms, year_built, buy_or_let)
+    VALUES
+    ($1, $2, $3, $4)
+    RETURNING *"
+    values = [@address, @no_of_bedrooms, @year_built, @buy_or_let]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]["id"].to_i()
+    db.close
+  end
+
+  def Property.all()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "SELECT * FROM property_tracker"
+    db.prepare("all", sql)
+    properties = db.exec_prepared("all")
+    db.close
+    return properties.map{|property| Property.new(property)}
+  end
+
+def delete_one()
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+  sql = "DELETE FROM property_tracker WHERE id = $1"
+  values = [@id]
+  db.prepare("delete_one", sql)
+  db.exec_prepared("delete_one", values)
+  db.close
+end
 
 
 
